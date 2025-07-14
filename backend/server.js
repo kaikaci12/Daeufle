5f87ec97db747a95ce1b266bbb9aa035808e9991
Tato Kurtsikidze <118304362+kaikaci12@users.noreply.github.com>
2025-07-14 12:45:26 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,7 @@ const admin = require("firebase-admin");
 const { getGeminiResponse } = require("./helpers/getGeminiResponse");
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
+  credential: admin.credential.cert(require('./serviceAccountKey.json')),
 });
 const verifyFirebaseToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
