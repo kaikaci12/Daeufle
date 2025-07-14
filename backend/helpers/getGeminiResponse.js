86a41f71609e2bd565145e6e247e23dd47e1d712
Tato Kurtsikidze <118304362+kaikaci12@users.noreply.github.com>
2025-07-14 13:40:01 +0400

    Update getGeminiResponse.js

--- backend/helpers/getGeminiResponse.js
+++ backend/helpers/getGeminiResponse.js
@@ -1,12 +1,4 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const geminiApiKey = process.env.GEMINI_API_KEY;
-if (!geminiApiKey) {
-  console.error("CRITICAL ERROR: GEMINI_API_KEY environment variable not set.");
-}
-const genAI = new GoogleGenerativeAI(geminiApiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-const getGeminiResponse = async (prompt) => {
+module.exports = async (prompt, model) => {
   try {
     const result = await model.generateContent({
       contents: [
@@ -39,4 +31,3 @@ const getGeminiResponse = async (prompt) => {
     throw error; // Re-throw the error for higher-level handling
   }
 };
-module.exports = { getGeminiResponse };
